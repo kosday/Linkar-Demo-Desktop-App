c03036ec6a52b1cfe165037bd2d0182ab04720b4
kosday <47530763+kosday@users.noreply.github.com>
2020-02-25 16:15:37 +0100

    First commit

---
+++ LINKARDEMO/LINKARDEMO/Forms/FormOrderDetails.cs
@@ -0,0 +1,108 @@
+﻿using LinkarClient;
+using LinkarCommon;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINKARDEMO
+{
+    public partial class FormOrderDetails : Form
+    {
+
+        LinkarClt _LinkarClt;
+
+        public FormOrderDetails(LinkarClt linkarClt)
+        {
+            _LinkarClt = linkarClt;
+            InitializeComponent();
+            txtHelp.Text = "In this form the \"Select\" method is used with \"onlyitems\" option to load the grid on the left.\r\nRecords are read every time they are focused using the \"Read\" method with the \"calculated\" option\r\nIn the select box you can write an order code to go directly to it\r\nTo load the data, a standard class is used that collects the MV buffer and assigns it to each property.";
+            cLkOrdersBindingSource.DataSource = new CLkOrders(this._LinkarClt);
+        }
+
+        #region -- Operations Bar
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            string filename = "";
+            string selectClause = this.txtSelect.Text;
+            string sortClause = "";
+            string usingDict = "";
+
+            int regPage = 0;
+            int numPag = 0;
+
+            CLkOrders clkorders = new CLkOrders(this._LinkarClt);
+            //Call to FindAll method of CLkOrders class
+            if (string.IsNullOrEmpty(selectClause))
+                clkorders.SelectAll(sortClause, filename, regPage, numPag, true, false, false);
+            else
+                clkorders.FindAll(sortClause, selectClause, regPage, numPag, filename, usingDict, true, false, false);
+
+            if (clkorders != null)
+            {
+                if (clkorders.LstErrors != null && clkorders.LstErrors.Count > 0)
+                {
+                    MessageBox.Show(string.Join("\r\n", clkorders.LstErrors));
+                }
+                //Draw customers data
+                cLkOrdersBindingSource.DataSource = clkorders;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            cLkOrdersBindingSource.DataSource = new CLkOrders(this._LinkarClt);
+        }
+
+        #endregion
+
+        #region -- Methods
+
+        private CLkOrder GetCurrentRecord()
+        {
+            CLkOrder record = null;
+            if (cLkOrdersBindingSource != null && cLkOrdersBindingSource.Current != null)
+                record = (CLkOrder)cLkOrdersBindingSource.Current;                           
+            return record;
+        }
+
+        private void DrawRecord(CLkOrder record)
+        {
+            if (record != null)
+            {
+                txtCode.Text = record.Code;
+                txtCustomer.Text = record.Customer;                
+                txtDate.Text = record.Date.HasValue ? record.Date.Value.ToShortDateString() : "";
+                txtTotalOrder.Text = record.ITotalOrder.ToString();
+                txtCustomerName.Text = record.ICustomerName;
+            }
+        }
+
+        #endregion
+
+        private void dgRecords_SelectionChanged(object sender, EventArgs e)
+        {
+            CLkOrder clkorder = GetCurrentRecord();
+            if (clkorder != null)
+            {
+                try
+                {
+                    clkorder.ReadRecord(true, "");
+                    DrawRecord(GetCurrentRecord());
+                    lstLstItemsBindingSource.ResetBindings(false);
+                }
+                catch (Exception ex)
+                {
+                    string msgErr = FormDemo.GetExceptionInfo(ex);
+                    MessageBox.Show(this, msgErr, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
---
+++ LINKARDEMO/LINKARDEMO/Forms/FormOrderLists.cs
@@ -0,0 +1,96 @@
+﻿using LinkarClient;
+using LinkarCommon;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LINKARDEMO
+{
+    public partial class FormOrderLists : Form
+    {
+        LinkarClt _LinkarClt;
+        int currentPage = 1;
+        int numRegforPag = 100;
+
+        public FormOrderLists(LinkarClt linkarClt)
+        {
+            _LinkarClt = linkarClt;
+            InitializeComponent();
+            txtHelp.Text = "In this form the \"Select\" method is used with \"calculated\" option to load the grid.\r\nIn the select box you can write an order code to go directly to it\r\nTo load the data, a standard class is used that collects the MV buffer and assigns it to each property.\r\nClicking on each line, you can view the Multivalue and Subvalue groups.";
+            cLkOrdersBindingSource.DataSource = new CLkOrders(this._LinkarClt);
+        }
+
+        #region -- Operations Bar
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            Select(1);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            cLkOrdersBindingSource.DataSource = new CLkOrders(this._LinkarClt);
+        }
+
+        #endregion
+
+        #region -- Methods
+
+        private void Select(int numPag)
+        {
+            string filename = "";
+            string selectClause = this.txtSelect.Text;
+            string sortClause = "";
+            string usingDict = "";
+
+            currentPage = numPag;
+
+            CLkOrders clkorders = new CLkOrders(this._LinkarClt);
+            //Call to FindAll or SelectAll method of CLkOrders class
+            if (string.IsNullOrEmpty(selectClause))
+                clkorders.SelectAll(sortClause, filename, numRegforPag, currentPage, false, true, true);
+            else
+                clkorders.FindAll(sortClause, selectClause, numRegforPag, currentPage, filename, usingDict, false, true, true);
+
+            if (clkorders != null)
+            {
+                if (clkorders.LstErrors != null && clkorders.LstErrors.Count > 0)
+                {
+                    MessageBox.Show(string.Join("\r\n", clkorders.LstErrors));
+                }
+                cLkOrdersBindingSource.DataSource = clkorders;
+            }
+        }
+
+        #endregion
+
+
+        private void cLkOrdersBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (currentPage - 1 > 0)
+                Select(currentPage - 1);
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            CLkOrders clkorders = (CLkOrders)cLkOrdersBindingSource.DataSource;
+            if (clkorders != null)
+            {
+                double t = (clkorders.totalRecords / numRegforPag);
+                if ((currentPage + 1) <= t)
+                    Select(currentPage + 1);
+            }
+        }
+    }
+}
